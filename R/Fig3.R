d36624608f543f8f5d2a5e13116457a1c4551aaa
Alberto Rovellini <alberto.rovellini@gmail.com>
2023-05-08 14:47:11 -0700

    Cleaned up WAA heatwave plotting code

--- R/Fig3.R
+++ R/Fig3.R
@@ -1,19 +1,10 @@
 # Alberto Rovellini
-# 3/21/2023
-# Code to create Fig. 3 for ICES paper
-# Fig. 3 weight at age and biomass of forage fish and groups that feed on it
+# 5/8/2023
+# Code to create heatmaps of relative change in weight at age over time
+# Keeping older code for absolute dynamics too
 # all the extraction code is take from Owen and PY's code
-
-# we need to compare all runs to the base
-
-# functional group dimensions
-# extract from base run
-# fg_dimensions <- hyper_grids(out_base) %>% 
-#   pluck("grid") %>% 
-#   purrr::map_df(function(x){
-#     out_base %>% activate(x) %>% hyper_vars() %>% 
-#       mutate(grd=x)
-#   })
+# 1. Temporal dynamics of weight at age
+# 2. Relative differences in weight at age among runs
 
 # apply waa plotting function
 fg_to_plot <- c('Capelin', 'Sandlance', 'Herring', 
@@ -21,42 +12,52 @@ fg_to_plot <- c('Capelin', 'Sandlance', 'Herring',
                 'Seabird_dive_fish', 'Seabird_surface_fish')
 
 waa_base <- bind_rows(purrr::map(fg_to_plot, plot_wage_timeseries, out = out_base, this.nc = this_nc_base, run = 'base'))
-waa_hw <- bind_rows(purrr::map(fg_to_plot, plot_wage_timeseries, out = out_hw, this.nc = this_nc_hw, run = 'hw'))
+waa_warm <- bind_rows(purrr::map(fg_to_plot, plot_wage_timeseries, out = out_warm, this.nc = this_nc_warm, run = 'warm'))
 waa_prod <- bind_rows(purrr::map(fg_to_plot, plot_wage_timeseries, out = out_prod, this.nc = this_nc_prod, run = 'prod'))
-waa_hw_prod <- bind_rows(purrr::map(fg_to_plot, plot_wage_timeseries, out = out_hw_prod, this.nc = this_nc_hw_prod, run = 'hw_prod'))
+waa_warm_prod <- bind_rows(purrr::map(fg_to_plot, plot_wage_timeseries, out = out_warm_prod, this.nc = this_nc_warm_prod, run = 'warm_prod'))
 
+
+# Line plot ---------------------------------------------------------------
+
+# These are probably less useful than the heatmap but they may be requested
+# Leave commented out but they should wok OK
 # # bind them and then plot them
-# waa_all <- rbind(waa_base, waa_hw)
+# waa_base_lines <- waa_base %>% mutate(run = 'base') %>% rename(weight = weight_base)
+# waa_warm_lines <- waa_warm %>% mutate(run = 'warm') %>% rename(weight = weight_warm)
+# waa_prod_lines <- waa_prod %>% mutate(run = 'prod') %>% rename(weight = weight_prod)
+# waa_warm_prod_lines <- waa_warm_prod %>% mutate(run = 'warm_prod') %>% rename(weight = weight_warm_prod)
+# 
+# waa_all <- rbind(waa_base_lines, waa_warm_lines, waa_prod_lines, waa_warm_prod_lines)
 # waa_all$Name <- factor(waa_all$Name, levels = fg_to_plot) # reorder groups
-# waa_all$age <- factor(waa_all$age, levels = as.character(1:10)) # treat age groups as a factor and order them 
+# waa_all$age <- factor(waa_all$age, levels = as.character(1:10)) # treat age groups as a factor and order them
 # 
 # # filter out the burn-in, or the worst of it at least
 # waa_all <- waa_all %>% filter(year >= 20)
 # 
+# # make labels
+# run_labs <- c('Control', 'Temperature + Plankton', 'Temperature', 'Plankton')
+# names(run_labs) <- c('base','warm_prod','warm','prod')
+# 
 # p <- ggplot()+
 #   geom_line(data = waa_all, aes(year,weight,col=age), linewidth = .8)+
 #   scale_color_viridis_d(begin = 0.1, end = 0.9)+
-#   geom_rect(data = data.frame(xmin = c(NA, 30),
-#                               xmax = c(NA, 35),
-#                               ymin = c(NA, -Inf),
-#                               ymax = c(NA, Inf),
-#                               run = c('Control','Heat wave')),
-#             aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax), color = NA, fill = 'red', alpha = 0.2)+
-#   theme_sleek()+
 #   labs(col="Age Group",y="Wet Weight per Individual (kg)",x="Year")+
-#   facet_grid2(run ~ Name, scales = 'free_y', independent = 'y')
+#   theme_bw()+
+#   facet_grid2(run ~ LongName, scales = 'free_y', independent = 'y', labeller = labeller(run = run_labs))
 # p
-# ggsave(paste0('output/', 'waa_', run_base, '_vs_', run_hw, '.png'),
+# ggsave(paste0('output/', 'waa_', run_base, '_vs_', run_warm, '.png'),
 #        p,width = 12,height=5)
 
-# heatmap for changes in condition
+
+# Heatmaps of relative difference in WAA over time ------------------------
+
 waa_heatmap_control <- waa_base
 
-waa_heatmap <- waa_hw %>%
-  left_join(waa_prod, by = c('year','age','age_group','Name')) %>%
-  left_join(waa_hw_prod, by = c('year','age','age_group','Name')) %>%
-  pivot_longer(-c(year, age_group, age, Name), names_to = 'run', values_to = 'weight_exp') %>%
-  left_join(waa_heatmap_control, by = c('year','age','age_group','Name')) %>%
+waa_heatmap <- waa_warm %>%
+  left_join(waa_prod, by = c('year','age','age_group','Name','LongName')) %>%
+  left_join(waa_warm_prod, by = c('year','age','age_group','Name','LongName')) %>%
+  pivot_longer(-c(year, age_group, age, Name, LongName), names_to = 'run', values_to = 'weight_exp') %>%
+  left_join(waa_heatmap_control, by = c('year','age','age_group','Name','LongName')) %>%
   mutate(percent_change = ((weight_exp-weight_base)/weight_base)*100) %>%
   filter(year >= 25)
 
@@ -73,31 +74,28 @@ waa_heatmap$age_group <- factor(waa_heatmap$age_group,
 
 # fix order of run
 waa_heatmap$run <- factor(waa_heatmap$run, 
-                                levels = c('weight_hw_prod','weight_hw','weight_prod'))
+                          levels = c('weight_warm_prod','weight_warm','weight_prod'))
 
 # add guild for facets 
 waa_heatmap <- waa_heatmap %>%
-  rowwise() %>%
-  mutate(Guild = case_when(
-    Name %in% c('Capelin', 'Sandlance', 'Herring') ~ 'Forage fish',
-    Name %in% c('Arrowtooth_flounder', 'Pollock', 'Cod') ~ 'Groundfish',
-    Name %in% c('Seabird_dive_fish', 'Seabird_surface_fish') ~ 'Seabirds'
-  )) %>%
-  ungroup()
+  left_join(guild_frame, by = c('Name' = 'fg')) 
+
+# fix order of guilds
+waa_heatmap$Guild <- factor(waa_heatmap$Guild,
+                            levels = c('Forage fish', 'Flatfish', 'Gadids', 'Seabirds'))
 
 # make labels
 run_labs <- c('Temperature + plankton', 'Temperature', 'Plankton')
-names(run_labs) <- c('weight_hw_prod','weight_hw','weight_prod')
+names(run_labs) <- c('weight_warm_prod','weight_warm','weight_prod')
 
 # plot
 waa_hm <- ggplot()+
   geom_tile(data = waa_heatmap, aes(x = year, y = age_group, fill = percent_change))+
   scale_fill_viridis()+
   geom_vline(xintercept = 30, color = 'red', linetype = 'dashed')+
-  geom_vline(xintercept = 35, color = 'red', linetype = 'dashed')+
   theme_bw()+
-  labs(x = 'Year', y = '', fill = '%', title = 'Relative change in weight at age from control run')+
-  facet_grid(Guild~run, scales = 'free_y', labeller = labeller(run = run_labs))
+  labs(x = 'Year', y = '', fill = '%')+#, title = 'Relative change in weight at age from control run')+
+  facet_grid(Guild~run, scales = 'free_y', space = 'free_y', labeller = labeller(run = run_labs))
 waa_hm
 
 ggsave(paste0('output/', 'waa_relchange.png'),
