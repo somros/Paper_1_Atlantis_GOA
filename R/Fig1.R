67a8e056d42348e1def49d6fd441c9e7ec5eaf4d
Alberto Rovellini <alberto.rovellini@gmail.com>
2023-05-08 14:02:43 -0700

    Moved plotting code for prod focrings to intro / methods

--- R/Fig1.R
+++ R/Fig1.R
@@ -154,6 +154,52 @@ p_summer_b <- ggplot()+
 p_summer_b
 ggsave(paste0('output/', 'goa_summer_sbt_', run_warm, '_vs_', run_base, '.png'), p_summer_b, width = 8, height = 3)
 
+
+# Productivity forcings ---------------------------------------------------
+
+# compare base to prod run
+# comparing biomass here even though the forcings are applied to growth
+# This is useful because it gives us a sense of the actual scaling of the biomass (is there more? less? for which groups?)
+biom_base <- read.table(paste0(dir_base, 'outputGOA0', run_base, '_testBiomIndx.txt'), header = T)
+biom_prod <- read.table(paste0(dir_prod, 'outputGOA0', run_prod, '_testBiomIndx.txt'), header = T)
+
+# keep columns of interest, reshape data
+biom_base_long <- biom_base %>%
+  select(Time:DR) %>%
+  mutate(Time = Time / 365) %>%
+  pivot_longer(-Time, names_to = 'Code', values_to = 'mt_base') %>%
+  left_join((grps %>% select(Code, Name)), by = 'Code') 
+
+biom_prod_long <- biom_prod %>%
+  select(Time:DR) %>%
+  mutate(Time = Time / 365) %>%
+  pivot_longer(-Time, names_to = 'Code', values_to = 'mt_prod') %>%
+  left_join((grps %>% select(Code, Name)), by = 'Code') 
+
+# join and get biomass relative to base run
+biom_all <- biom_base_long %>%
+  left_join(biom_prod_long, by = c('Time','Code','Name')) %>%
+  mutate(Control = mt_base / mt_base,
+         `Heat wave` = mt_prod / mt_base) %>%
+  select(Time, Code, Name, Control, `Heat wave`) %>%
+  pivot_longer(-c(Time, Code, Name), names_to = 'Run', values_to = 'rel_biomass')
+
+# plot biomass over time, keep only plankton groups that we are changing
+p_prod <- biom_all %>%
+  filter(Code %in% c('ZM','PL')) %>%
+  ggplot(aes(x = Time, y = rel_biomass, group = Run))+
+  geom_line(aes(color = Run, linetype = Run), linewidth = 0.8)+
+  scale_color_viridis_d(begin = 0.2, end = 0.8)+
+  annotate("rect", xmin = 30, xmax = 35, ymin = -Inf, ymax = Inf,
+           alpha = .2, fill = 'yellow')+
+  theme_bw()+
+  labs(x = 'Year', y = 'Biomass relative to control')+
+  facet_wrap(~Name, ncol = 1)
+p_prod
+
+ggsave(paste0('output/', 'relative_plankton_biomass_', run_base, '_vs_', run_prod, '.png'), 
+       p_prod, width = 5, height = 1.5, dpi = 600)
+
 # CEATTLE -----------------------------------------------------------------
 
 # Bioenergetics from CEATTLE
@@ -244,7 +290,7 @@ make_niche <- function(min_sp, max_sp, current_enviro, K_const_sp = k){
 dat1 <- dat %>%
   rename(mint = min, maxt = max)
 
-# atach temperature as column
+# attach temperature as column
 dat2 <- do.call("rbind", replicate(length(temp), dat1, simplify = FALSE)) %>%
   arrange(Index) %>%
   mutate(temp = rep(temp, nrow(dat)))
@@ -269,7 +315,6 @@ dat_vline <- dat3 %>%
 dat_vline$edge <- gsub('mint', 'Minimum', dat_vline$edge)
 dat_vline$edge <- gsub('maxt', 'Maximum', dat_vline$edge)
 
-         
 # plot
 p_niche <- dat3 %>%
   filter(Name %in% to_show)%>%
